459b10e4c0814821526f406f7ec7b3cbe226828e
Éverton Araújo <araujocostaeverton@gmail.com>
2023-11-06 08:25:35 -0300

    Setup inicial wallet

---
+++ src/main/java/br/com/invillia/cdb/wallet/WalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.invillia.cdb.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class, args);
+    }
+
+}
